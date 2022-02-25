96274c3421e6b13b52b02a573005de763241fcc0
StephenJoge <15234189615@163.com>
2022-02-25 18:21:10 +0800

    update

---
+++ excelFunc/excelFileTest.go
@@ -0,0 +1,95 @@
+package excelFunc
+
+import (
+	"fmt"
+
+	"github.com/xuri/excelize/v2"
+)
+
+//表创建
+func CreateExcelFile() {
+	f := excelize.NewFile()
+	// 创建一个工作表
+	index := f.NewSheet("Sheet2")
+	// 设置单元格的值
+	f.SetCellValue("Sheet2", "A2", "Hello world.")
+	f.SetCellValue("Sheet1", "B2", 100)
+	// 设置工作簿的默认工作表
+	f.SetActiveSheet(index)
+	// 根据指定路径保存文件
+	if err := f.SaveAs("Book1.xlsx"); err != nil {
+		fmt.Println(err)
+	}
+}
+
+//表读取
+func ReaderExcelFile() {
+	f, err := excelize.OpenFile("Book1.xlsx")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// 获取工作表中指定单元格的值
+	cell, err := f.GetCellValue("Sheet1", "B2")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(cell)
+	// 获取 Sheet1 上所有单元格
+	rows, err := f.GetRows("Sheet2")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, row := range rows {
+		for _, colCell := range row {
+			fmt.Print(colCell, "\t")
+		}
+		fmt.Println()
+	}
+}
+
+func CreateEchart() {
+	categories := map[string]string{
+		"A2": "Small", "A3": "Normal", "A4": "Large", "B1": "Apple", "C1": "Orange", "D1": "Pear"}
+	values := map[string]int{
+		"B2": 2, "C2": 3, "D2": 3, "B3": 5, "C3": 2, "D3": 4, "B4": 6, "C4": 7, "D4": 8}
+	f := excelize.NewFile()
+	for k, v := range categories {
+		f.SetCellValue("sheet1", k, v)
+	}
+	for k, v := range values {
+		f.SetCellValue("sheet1", k, v)
+	}
+	if err := f.AddChart("Sheet1", "E1", `{
+        "type": "col3DClustered",
+        "series": [
+        {
+            "name": "Sheet1!$A$2",
+            "categories": "Sheet1!$B$1:$D$1",
+            "values": "Sheet1!$B$2:$D$2"
+        },
+        {
+            "name": "Sheet1!$A$3",
+            "categories": "Sheet1!$B$1:$D$1",
+            "values": "Sheet1!$B$3:$D$3"
+        },
+        {
+            "name": "Sheet1!$A$4",
+            "categories": "Sheet1!$B$1:$D$1",
+            "values": "Sheet1!$B$4:$D$4"
+        }],
+        "title":
+        {
+            "name": "Fruit 3D Clustered Column Chart"
+        }
+    }`); err != nil {
+		fmt.Println(err)
+		return
+	}
+	// 根据指定路径保存文件
+	if err := f.SaveAs("Book2.xlsx"); err != nil {
+		fmt.Println(err)
+	}
+}
--- excelProject/go.mod
+++
@@ -1,7 +0,0 @@
-module excelProject
-
-go 1.17
-
-require github.com/360EntSecGroup-Skylar/excelize v1.4.1
-
-require github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
